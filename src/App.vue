9d588ceedd5edd39f4b8b2162ca144c42f8df812
verayifan <1207619566@qq.com>
2023-04-27 17:02:00 +0800

    添加接口

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import axios from 'axios'
+axios.get('https://www.baidu.com')
+    .then((response: { data: any; }) => {
+        console.log(response.data)
+    })
+    .catch((error: any) => {
+        console.log(error)
+    })
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">七的衣柜</RouterLink>
-      </nav>
-    </div>
+      <div class="wrapper">
+          <nav>
+              <RouterLink to="/">Op</RouterLink>
+              <RouterLink to="/image">Jsk</RouterLink>
+              <RouterLink to="/image">Sk</RouterLink>
+              <RouterLink to="/image">内搭</RouterLink>
+              <RouterLink to="/image">外搭</RouterLink>
+          </nav>
+      </div>
   </header>
 
-  <RouterView />
+    <RouterView />
 </template>
